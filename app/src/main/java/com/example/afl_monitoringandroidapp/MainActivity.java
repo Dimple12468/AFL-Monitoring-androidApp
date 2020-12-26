1a5b7dfa459aa81581da745dca12d63885f580b6
Natasha Choudhary <natashasee.2001@gmail.com>
2020-12-26 18:19:45 +0530

    Splash screen java file

--- app/src/main/java/com/example/afl_monitoringandroidapp/MainActivity.java
+++ app/src/main/java/com/example/afl_monitoringandroidapp/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.afl_monitoringandroidapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /*
+                SharedPreferences preferences = getSharedPreferences("tokenFile", Context.MODE_PRIVATE);
+                String typeofuser = preferences.getString("role","");
+                String username = preferences.getString("Name","");
+                Intent i;
+                if(typeofuser.isEmpty()){
+                    i = new Intent( splash_page.this ,Initial_page.class);
+                }else
+                    i = new Intent( splash_page.this ,login_activity.class);
+                */
+                Intent i;
+                 i = new Intent( splash_page.this ,InitialPage.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
     }
 }
