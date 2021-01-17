11ddbf922c81994f5329794e6ab03a211aa70af6
Natasha2001 <natashasee.2001@gmail.com>
2021-01-17 14:16:24 +0530

    ado tab added

--- app/src/main/java/com/example/afl_monitoringandroidapp/adminTabs/adminDda.java
+++ app/src/main/java/com/example/afl_monitoringandroidapp/adminTabs/adminDda.java
@@ -1,5 +1,8 @@
 package com.example.afl_monitoringandroidapp.adminTabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -10,8 +13,10 @@ import androidx.lifecycle.ViewModelProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.afl_monitoringandroidapp.InitialPage;
 import com.example.afl_monitoringandroidapp.R;
 
 public class adminDda extends Fragment {
@@ -35,6 +40,21 @@ public class adminDda extends Fragment {
                 textView.setText(s);
             }
         });
+
+        //for logout
+        Button button = root.findViewById(R.id.logOut_ado);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getActivity().getSharedPreferences("tokenFile", Context.MODE_PRIVATE).edit();
+                editor.clear();
+                editor.commit();
+                Intent intent = new Intent(getActivity(), InitialPage.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
         return root;
     }
 }
